b122a5d69ecc57a8c59cf095888e8858d687550d
Seigneur-Machiavel <guibi50ae@hotmail.fr>
2023-09-21 18:41:33 +0200

    log launch_folder

--- onepage-node-ejs/server.js
+++ onepage-node-ejs/server.js
@@ -40,7 +40,7 @@ for (let i = 0; i < args.length; i++) {
 //#endregion ----------------------------------------------
 
 //#region - IMPORTS - MODULES - SCRIPTS PUBLIFICATION
-const launch_folder = is_debug ? "" : __dirname.split('\\').pop().split('/').pop();
+const launch_folder = is_debug ? "" : __dirname.split('\\').pop().split('/').pop(); console.log(`launch_folder: ${launch_folder}`); // Get the name of the folder where the server is launched
 const fs = require('fs');
 const path = require('path');
 const express = require('express');
